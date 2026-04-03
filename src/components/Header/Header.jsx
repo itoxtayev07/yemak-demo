ea072ea46c4109a5468c8449758456c63b1da0c6
Ibrohim Tuxtayev <itoxtayev07@gmail.com>
2026-04-03 09:48:08 +0500

    fix: Header fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -14,7 +14,7 @@ export function Header() {
     return <header className="header w-full max-w-full !px-[24px] sticky top-0 z-[10] flex justify-center items-center bg-[#FFF]">
         <section className="header-sect w-full max-w-[1440px] !py-[18px] flex justify-between items-center gap-[16px]">
             <section className="search-sect w-full max-w-[740px] flex justify-between items-center">
-                <Link to={'/'}><img src={logo} /></Link>
+                <Link className='cursor-pointer' to={'/'}><img src={logo} /></Link>
 
                 <label className="search-product-wrap w-full max-w-[300px] flex justify-between items-center gap-[12px] border-[1px] border-[#F0F0F0] rounded-[10px] !py-[11px] !px-[10px] bg-[#F7F7F7] transition-all duration-[.3s] focus-within:border-[#EDC843]">
                     <div className="flex justify-center items-center gap-[8px]">
@@ -36,21 +36,21 @@ export function Header() {
             </section>
             <section className="navbar-sect w-full max-w-[294px] flex justify-between items-center gap-[12px]">
                 <ul className='w-full max-w-[228px] flex justify-between items-center gap-[8px]'>
-                    <li className="link flex flex-col items-center">
+                    <li className="link flex flex-col items-center cursor-pointer">
                         <img src={receipt} />
                         <p className='font-[600] text-[12px] leading-none tracking-normal text-center'>Buyurtmalar</p>
                     </li>
-                    <li className="link flex flex-col items-center">
+                    <li className="link flex flex-col items-center cursor-pointer">
                         <img src={basket} />
                         <p className='font-[600] text-[12px] leading-none tracking-normal text-center'>Savatcha</p>
                     </li>
-                    <li className="link flex flex-col items-center">
+                    <li className="link flex flex-col items-center cursor-pointer">
                         <img src={world} />
                         <p className='font-[600] text-[12px] leading-none tracking-normal text-center'>O’zbekcha</p>
                     </li>
                 </ul>
 
-                <div className="user min-w-[42px] min-h-[42px] rounded-full bg-[#F7F7F7] border-[1.5px] flex justify-center items-center border-[#F0F0F0]">
+                <div className="user min-w-[42px] min-h-[42px] rounded-full bg-[#F7F7F7] border-[1.5px] flex justify-center items-center border-[#F0F0F0] cursor-pointer">
                     <img src={user} />
                 </div>
             </section>
